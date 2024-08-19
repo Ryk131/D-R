03ca65cbd2a5a361098289252d497ae936735d36
Ryk131 <rykelmy131@gmail.com>
2024-08-19 19:26:35 -0300

    aa

--- app/cont_regr/contagem.php
+++ app/cont_regr/contagem.php
@@ -12,6 +12,25 @@ $diferenca_minutos = $diferenca_horas % 60;
 $dias = ($diferenca_total - $diferenca_dias) / 86400;
 $horas = ($diferenca_dias - $diferenca_horas) / 3600;
 $minutos = ($diferenca_horas - $diferenca_minutos) / 60;
+$segundos = $diferenca_total
+
+echo "<div>";
+    echo "<p>".$dias."</p>";
+    echo "<a>Dias</a>";
+echo "</div>";
+echo "<div>";
+    echo "<p>:</p>";
+echo "</div>";
+echo "<div>";
+    echo "<p>".$horas."</p>";
+    echo "<a>Horas</a>";
+echo "</div>";
+echo "<div>";
+    echo "<p>:</p>";
+echo "</div>";
+    echo "<p>".$minutos."</p>";
+    echo "<a>Minutos</a>";
+echo "</div>";
 
 echo "<p>".$dias." : ".$horas." : ".$minutos."</p>";
 echo "<a>Dias</a>
